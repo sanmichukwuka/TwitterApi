ee35e9a045c1647de6e975f83a896ca47b23e173
sanmichukwuka <michaelsanmi09@gmail.com>
2020-12-16 17:48:33 -0600

    first commit

---
+++ src/main/java/com/tts/TransitApp/TwitterApi3Application.java
@@ -0,0 +1,13 @@
+package com.tts.TransitApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterApi3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterApi3Application.class, args);
+	}
+
+}
